1dcaba9c682afbbd8efbea17dc541c4c841c3f78
MidhileshMajety <midhileshmajeyti@gmail.com>
2018-03-22 21:10:46 +0530

    Initial commit

---
+++ app/src/main/java/com/milli/picit/splashscreenactivity.java
@@ -0,0 +1,10 @@
+package com.milli.picit;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ASUS on 22-03-2018.
+ */
+
+public class splashscreenactivity extends AppCompatActivity {
+}
