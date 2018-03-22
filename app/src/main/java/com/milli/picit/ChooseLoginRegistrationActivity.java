0ab8c8c81958d1c17ddb5cb80a65ccb734ae8148
MidhileshMajety <midhileshmajeyti@gmail.com>
2018-03-23 00:07:38 +0530

    go

--- app/src/main/java/com/milli/picit/ChooseLoginRegistrationActivity.java
+++ app/src/main/java/com/milli/picit/ChooseLoginRegistrationActivity.java
@@ -1,7 +1,10 @@
 package com.milli.picit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChooseLoginRegistrationActivity extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class ChooseLoginRegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_login_registration);
+
+        Button mLogin = findViewById(R.id.login);
+        Button mRegistration = findViewById(R.id.registration);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), LoginActivity.class);
+                startActivity(intent);
+                return;
+
+            }
+        });
+
+        mRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplication(), RegistrationActivity.class);
+                startActivity(intent);
+                return;
+
+
+            }
+        });
+
     }
 }
